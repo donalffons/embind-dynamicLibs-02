c06a335e2b4a5673990c07eaa4298f6ac0169b17
Sebastian Alff <donalffons@gmail.com>
2020-12-16 00:24:26 +0100

    strip includes

--- emscripten/library1-sources/Standard.cxx
+++ emscripten/library1-sources/Standard.cxx
@@ -1,39 +1,12 @@
-// Created on: 2005-03-15
-// Created by: Peter KURNEV
-// Copyright (c) 1998-1999 Matra Datavision
-// Copyright (c) 1999-2014 OPEN CASCADE SAS
-//
-// This file is part of Open CASCADE Technology software library.
-//
-// This library is free software; you can redistribute it and/or modify it under
-// the terms of the GNU Lesser General Public License version 2.1 as published
-// by the Free Software Foundation, with special exception defined in the file
-// OCCT_LGPL_EXCEPTION.txt. Consult the file LICENSE_LGPL_21.txt included in OCCT
-// distribution for complete text of the license and disclaimer of any warranty.
-//
-// Alternatively, this file may be used under the terms of Open CASCADE
-// commercial license or contractual agreement.
-
-
-#include <Standard.hxx>
-#include <Standard_MMgrOpt.hxx>
-#include <Standard_MMgrRaw.hxx>
-#include <Standard_MMgrTBBalloc.hxx>
-#include <Standard_Assert.hxx>
-
-#include <stdlib.h>
-#include <mm_malloc.h>
-
 class Standard_MMgrFactory
 {
 public:
-  static Standard_MMgrRoot* GetMMgr();
+  static int GetMMgr();
   ~Standard_MMgrFactory(){}
 };
 
-
-Standard_MMgrRoot* Standard_MMgrFactory::GetMMgr()
+int Standard_MMgrFactory::GetMMgr()
 {
   static Standard_MMgrFactory aFactory;
-  return NULL;
+  return 1;
 }
